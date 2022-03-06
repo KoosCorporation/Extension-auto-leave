89326b344c93d9623e71ce6150919e876064f4f7
Edgardo Valencia <edgardo.avalencia@gmail.com>
2022-03-06 17:56:25 -0600

    Se limpio

--- popup.js
+++ popup.js
@@ -2,36 +2,44 @@ var checkparticipants = document.getElementById("activate");
 var deactivate = document.getElementById("deactivate");
 var spanWrite = document.getElementById("writeP")
 
-checkparticipants.addEventListener("click", async()=>{
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: checkingParticipants,
+checkparticipants.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+  chrome.scripting.executeScript({
+    target: {
+      tabId: tab.id
+    },
+    function: checkingParticipants,
   });
 });
 
 function checkingParticipants() {
-    chrome.storage.local.get("active", ({ active }) => {
-      if (active == "false"){
-        console.log("Activating")
-        chrome.storage.local.set({ active:"true" });
-        setInterval(() => {
-          var divParticipants = document.getElementsByClassName("uGOf1d")
-          var number  = divParticipants[0].innerHTML
-          console.log(number)
-          if (number < 3){
-            var hangoutbuttonclassname = "VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ tWDL4c  jh0Tpd Gt6sbf QQrMi NKaD6"
-            var hangoutbutton = document.getElementsByClassName(hangoutbuttonclassname)[0]
-            hangoutbutton.click();
-          }
-        }, 1000);
-      }
-      else if (active == "true"){
-        console.log("Already active")
-      }
-    });
-}
+  chrome.storage.local.get("active", ({
+    active
+  }) => {
+    if (active == "false") {
+      console.log("Activating")
+      chrome.storage.local.set({
+        active: "true"
+      });
+      setInterval(() => {
+        var divParticipants = document.getElementsByClassName("uGOf1d")
+        var number = divParticipants[0].innerHTML
+        console.log(number)
+        if (number < 3) {
+          var hangoutbuttonclassname = "google-material-icons VfPpkd-kBDsod r6Anqf"
 
-  function noth(){
-    console.log("nothing")
-  }
+          var hangoutbutton = document.getElementsByClassName(hangoutbuttonclassname)[0]
+          chrome.storage.local.set({
+            active: "false"
+          });
+          hangoutbutton.click();
+        }
+      }, 1000);
+    } else if (active == "true") {
+      console.log("Already active")
+    }
+  });
+}
