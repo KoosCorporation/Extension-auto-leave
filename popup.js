6563abef8d879f8aba0cc6cd4d62998f4cbd88e1
frank <francortizm238@gmail.com>
2022-03-07 16:38:43 -0600

    for switch branch

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ checkparticipants.addEventListener("click", async () => {
     currentWindow: true
   });
 
-  //TOMA DE NUMERO MINIMO DE PARTICIPANTES INGRESADOS POR EL USUARIO
+  //-------------TOMA DE NUMERO MINIMO DE PARTICIPANTES INGRESADOS POR EL USUARIO
   var NumMin = inputNumMin.value
   if (NumMin < 1) {
     spanWrite.innerHTML = "No puede ser inferior a 1"
@@ -23,7 +23,7 @@ checkparticipants.addEventListener("click", async () => {
         args: [NumMin]
       },
       (injectionResults) => {
-        //for (const frameResult of injectionResults)
+        //-------------for (const frameResult of injectionResults)
         if (injectionResults[0].result) {
           spanWrite.innerHTML = "Activado"
         } else {
@@ -47,7 +47,7 @@ deactivate.addEventListener("click", async () => {
       function: deactivating
     },
     (injectionResults) => {
-      //for (const frameResult of injectionResults)
+      //-------------for (const frameResult of injectionResults)
       if (injectionResults[0].result) {
         spanWrite.innerHTML = "Desactivado"
       } else {
@@ -63,26 +63,37 @@ function checkingParticipants(NumMin) {
   let divParticipants = document.getElementsByClassName("uGOf1d")[0]
   let hangoutbuttonclassname = "google-material-icons VfPpkd-kBDsod r6Anqf"
   let hangoutbutton = document.getElementsByClassName(hangoutbuttonclassname)[0]
-  // VERIFIACION DE ENCUENMTRO DE ELEMENTOS PARA CONTROL 
+  // -------------VERIFIACION DE ENCUENMTRO DE ELEMENTOS PARA CONTROL 
   var toreturn = ''
   if (divParticipants != undefined & hangoutbutton != undefined) {
     console.log("todo bien")
-    // TOMA LA VARIABLE ACTIVE DE STORAGE PARA VERIFICAR SI ESTA YA EN FUNCIONAMIENTO EL BOT
+    // -------------TOMA LA VARIABLE ACTIVE DE STORAGE PARA VERIFICAR SI ESTA YA EN FUNCIONAMIENTO EL BOT
     chrome.storage.local.get("active", ({
       active
     }) => {
-      // VERIFICACION DE ACTIVIDAD
+      // -------------VERIFICACION DE ACTIVIDAD
       if (active == "false") {
         console.log("Activating")
         chrome.storage.local.set({
           active: "true"
         });
+        //-------------NOTIFICACION DE ACTIVACION
+        chrome.notifications.create(
+          "name",
+          {
+            type: "basic",
+            iconUrl: "images/image.jpg",
+            title: "This is a notification",
+            message: "hello there!",
+          },
+          function () {}
+        );
         // INTERVALO DE 1 SEGUNDO PARA VERIFICAR LOS PARTICIPANTES CONSTANTEMENTE
         var intervalo = setInterval(() => {
           let divParticipants = document.getElementsByClassName("uGOf1d")
           let number = divParticipants[0].innerHTML
           console.log(number)
-          // VERIFICACION DE SOLICITUD DE INACTIVIDAD
+          // -------------VERIFICACION DE SOLICITUD DE INACTIVIDAD
           chrome.storage.local.get("active", ({
             active
           }) => {
@@ -90,17 +101,17 @@ function checkingParticipants(NumMin) {
               clearInterval(intervalo)
             }
           })
-          // CONTROL DE NUMERO MINIMO DE PARTICIPANTES ANTES DE COLGAR LA LLAMADA
+          // -------------CONTROL DE NUMERO MINIMO DE PARTICIPANTES ANTES DE COLGAR LA LLAMADA
           if (number < NumMin) {
             let hangoutbuttonclassname = "google-material-icons VfPpkd-kBDsod r6Anqf"
             let hangoutbutton = document.getElementsByClassName(hangoutbuttonclassname)[0]
-            // SETEO DE STORAGE PARA DETENCION DE ACTIVIDAD
+            // -------------SETEO DE STORAGE PARA DETENCION DE ACTIVIDAD
             chrome.storage.local.set({
               active: "false"
             });
-            // CUELGA LA LLAMADA
+            // -------------CUELGA LA LLAMADA
             hangoutbutton.click();
-            // ENVIO DE MENSAJE A TELEGRAM COMO NOTIFICACION
+            // -------------ENVIO DE MENSAJE A TELEGRAM COMO NOTIFICACION
             var token = "5217446941:AAFp8Iiw_Nl8I3IqExyku4CmVSE74-jgnts"
             var chatid = "1144214477"
             var text = "Ya fuera de la reunion"
@@ -111,13 +122,13 @@ function checkingParticipants(NumMin) {
           }
         }, 1000);
       } else if (active == "true") {
-        // CONTROL DE ESTADO DE ACTIVIDAD
+        // -------------CONTROL DE ESTADO DE ACTIVIDAD
         console.log("Already active")
       }
     });
     toreturn = true
   } else {
-    // CONTROL DE NOT FOUND PARA COMPONENTES NECESARIOS
+    // -------------CONTROL DE NOT FOUND PARA COMPONENTES NECESARIOS
     console.log("no definidos")
     toreturn = false
   }
